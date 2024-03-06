985ea54570417fe3f097aa060f5aa2b4cca9b727
Sovietroll <flylusarms@gmail.com>
2024-03-07 01:38:27 +0800

    home

--- prototypeShelf.js
+++ prototypeShelf.js
@@ -1,8 +1,19 @@
 let data = [];
 
-  let quantitySupplement = 0;
+  let quantitySupplement_A = 0;
+  let quantitySupplement_B = 0;
+  let quantitySupplement_C = 0;
+  let quantitySupplement_D = 0;
+  let quantitySupplement_E = 0;
+  let quantitySupplement_F = 0;
+  let quantitySupplement_G = 0;
+  let quantitySupplement_H = 0;
+  let quantitySupplement_I = 0;
+  let quantitySupplement_J = 0;
+  let quantitySupplement_K = 0;
+  let quantitySupplement_L = 0;
 
-  let toggleEditable = true;
+  let toggleEditable;
 
   let output = document.querySelector('.outputHere');
   
@@ -25,44 +36,100 @@ let data = [];
       })
       },console.log(`deleted`,data)
       );
- };
+    };
 
 
-  let inputOne = document.querySelector(".js-one-D");
+  const inputOne = document.querySelectorAll('.js-one-D, .js-one-B');
 
-  const input = document.querySelector('.js-input-D');
-  input.style.display = "none"; 
+  const inputB = document.querySelector('.js-input-B');
+  const inputD = document.querySelector('.js-input-D');
 
-  inputOne.addEventListener('click', appearInputOne);
+  inputD.style.display = "none"; 
+  inputB.style.display = "none"; 
 
-  function appearInputOne(){
-  input.style.display = "block"; 
-  toggleEditable = false;
-  inputOne.innerHTML = '';
-  }; 
-  
+  inputOne.forEach(element => {
+    element.addEventListener('click',appearInputOne);
+  });
+  /*
+  inputOne.addEventListener('click', appearInputOne); 
+  */
+ 
+ function appearInputOne(){
+  if (toggleEditable === 2){
+    inputB.style.display = "block"; 
+    inputD.style.display = "none"; 
+    toggleEditable = false;
+    inputOne.innerHTML = '';
+  }
+  else if (toggleEditable === 4){
+   inputD.style.display = "block"; 
+   inputB.style.display = "none"; 
+   toggleEditable = false;
+   innerHTML = '';
+  }
+}; 
+
   const bodyEvent = document.body.addEventListener('keydown',(event) => {
     if(event.key === 'Enter'){
+      if (toggleEditable === 2){
       data.push({
-        name: input.value,
-        quantity: quantitySupplement //quantity here permanent numbers 
+            name: inputB.value,
+            quantity: quantitySupplement_B
       });
-      inputOne.innerHTML += input.value; //inputOne innerHTML
+    } 
+    else if (toggleEditable === 4){
+      data.push({
+        name: inputD.value,
+        quantity: quantitySupplement_D
+       });
+    }
+      inputOne.innerHTML += inputD.value,inputB.value;
 
-      input.value = '';
+      inputB.value = '';
+      inputD.value = '';
+
       quantitySupplement = 0;
-      input.style.display = "none";
+
+      if (toggleEditable === 2){
+        inputB.style.display = "none"; 
+      }
+      else if (toggleEditable === 4){
+        inputD.style.display = "none";
+      }
       runData();
       console.log(data);
     } 
-    
-    });
+   }
+  );
    
+  let toggleSect_B;
 
   let shelfQuantity_B = document.querySelector('.js-quantity-B');
 
   let shelfQuantity_D = document.querySelector('.js-quantity-D');
 
+  const shelf_B = document.querySelector('.shelf-B')
+  shelf_B.addEventListener('click', () => {
+    const hightLight_B = () => {
+      toggleSect_B = true;
+      shelf_B.classList.add("shelf-B-active");
+      shelf_D.classList.remove("shelf-B-active");
+      toggleEditable = 2;
+    };
+    hightLight_B();
+  });
+
+  const shelf_D = document.querySelector('.shelf-D');
+  shelf_D.addEventListener('click', () => {
+    const hightLight_D = () => {
+      toggleSect_B = false;
+      shelf_D.classList.add("shelf-B-active");
+      shelf_B.classList.remove("shelf-B-active");
+      toggleEditable = 4;
+
+    };
+    hightLight_D();
+  })
 
   shelfQuantity_D.value = '';
   shelfQuantity_B.value = '';
@@ -78,11 +145,19 @@ let data = [];
   }
   )
   function buttonPlus(){
-    quantitySupplement++;
-    shelfQuantity_B.innerHTML = `${quantitySupplement}`;
-    console.log('plus');
-    console.log(quantitySupplement);
-
+    if (toggleSect_B === true){
+    quantitySupplement_B++;
+    shelfQuantity_B.innerHTML = `${quantitySupplement_B}`;
+    console.log(quantitySupplement_B);
+    } 
+    else if (toggleSect_B === false){
+    quantitySupplement_D++;
+    shelfQuantity_D.innerHTML = `${quantitySupplement_D}`;
+    console.log(quantitySupplement_D);
+    }
+    else {
+      alert('none select');
+    }
   };
 
   const buttonSubtract = document.querySelectorAll('.js-button-minus-B, .js-button-minus-D');
@@ -92,13 +167,17 @@ let data = [];
     })
 
     function buttonMinus(){
-      quantitySupplement > 0?
-      (quantitySupplement--,
-      shelfQuantity_D.innerHTML = `${quantitySupplement}`,
-      console.log('minus'),
-     console.log(quantitySupplement)
-      )
-      : 
-      alert('Reach Limit');
+      if (toggleSect_B === true && quantitySupplement_B > 0){
+        quantitySupplement_B--;
+        shelfQuantity_B.innerHTML = `${quantitySupplement_B}`;
+        console.log(quantitySupplement_B);
+      } 
+      else if (toggleSect_B === false && quantitySupplement_D > 0){
+        quantitySupplement_D--;
+        shelfQuantity_D.innerHTML = `${quantitySupplement_D}`;
+        console.log(quantitySupplement_D);
+      }
+      else {
+        alert('Reach Limit');
     }
-
+  };
