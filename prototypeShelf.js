d6e71bd6fb75100755bf7c8533a0a335482c98ad
Sovietroll <flylusarms@gmail.com>
2024-03-10 01:52:52 +0800

    HOME

--- prototypeShelf.js
+++ prototypeShelf.js
@@ -1,11 +1,13 @@
 let data = [];
 
-  let quantitySupplement_A = 0;  //1
-  let quantitySupplement_A2 = 0; //1.1
-  let quantitySupplement_B1 = 0;  //2
-  let quantitySupplement_B2 = 0; //2.1
-  let quantitySupplement_C = 0;
-  let quantitySupplement_D = 0;
+  let quantitySupplement_A1 = 0;//1
+  let quantitySupplement_A2 = 0;//1.1
+  let quantitySupplement_B1 = 0;//2
+  let quantitySupplement_B2 = 0;//2.1
+  let quantitySupplement_C1 = 0;//3
+  let quantitySupplement_C2 = 0;//3.1
+  let quantitySupplement_D = 0;//4
+  let quantitySupplement_D2 = 0;//4.1
   let quantitySupplement_E = 0;
   let quantitySupplement_F = 0;
   let quantitySupplement_G = 0;
@@ -15,19 +17,56 @@ let data = [];
   let quantitySupplement_K = 0;
   let quantitySupplement_L = 0;
 
+  // TOGGLE ARRAY
+  const shelf = {
+    B1: 'B1',
+    B2: 'B2',
+    D: 'D',
+    D2: 'D2'
+    };
+  
+
   let toggleEditable;
 
   let output = document.querySelector('.outputHere');
 
+  //NUMBERS POP UP
   let numberPanel = document.querySelector('.js-panelNumber');
+  let togglePanelOnOff = true;
 
-  document.querySelector('.quantityDisplay').addEventListener('click', () => {
-    numberPanel.style.display ="block";
-    console.log('click')
-    numberPanel.classList.add("css-panelNumber-active");
+  const numbersD2 = document.querySelector('.quantityDisplay');
+
+  let enterQuantity = document.querySelector('.js-EnterPanelNumber');
+  enterQuantity.addEventListener('click', ()=> {
+    enterQuantity.value = quantitySupplement_D2;
+
+    console.log(enterQuantity.value);
+    console.log(data);
+    numbersD2.innerHTML = `${enterQuantity.value}`;
+
+    data.push({
+      name: 'test',
+      quantity: enterQuantity.value
+    });
+    runData();
+  });
+
+  numbersD2.addEventListener('click', () => {
+      togglePanelOnOff? (
+      console.log('on numberPanel'),
+      numberPanel.classList.add("css-panelNumber-active"),
+      numberPanel.classList.remove("css-panelNumber"),
+      togglePanelOnOff = false
+      )
+      :
+      (
+      console.log('off numberPanel'),
+      numberPanel.classList.add("css-panelNumber"),
+      numberPanel.classList.remove("css-panelNumber-active"),
+      togglePanelOnOff = true 
+      )
   })
 
-  numberPanel.style.display = "block"
   
   function runData(){
   output.innerHTML = '';
@@ -49,41 +88,50 @@ let data = [];
       );
     };
 
+  //STATEMENT ITEM HERE
+  const OneB1 = document.querySelector('.js-one-B1');
+  const OneB2 = document.querySelector('.js-one-B2');
+  const OneD = document.querySelector('.js-one-D');
+  const OneD2 = document.querySelector('.js-one-D2');
+  
+  const inputs = [OneB1, OneB2, OneD, OneD2];
+  inputs.forEach(element => {
+    element.addEventListener('click',appearInputOne);
+  })
 
-  const inputOneB1 = document.querySelector('.js-one-B1');
-  const inputOneB2 = document.querySelector('.js-one-B2');
-  const inputOneD = document.querySelector('.js-one-D');
-
-
+  //INPUT HERE
   const inputB1 = document.querySelector('.js-input-B1');
   const inputB2 = document.querySelector('.js-input-B2');
   const inputD = document.querySelector('.js-input-D');
-
-
-  inputOneB1.addEventListener('click',appearInputOne);
-  inputOneB2.addEventListener('click',appearInputOne);
-  inputOneD.addEventListener('click',appearInputOne);
+  const inputD2 = document.querySelector('.js-input-D2');
   
   inputD.style.display = "none"; 
+  inputD2.style.display = "none";
   inputB1.style.display = "none"; 
   inputB2.style.display = "none"; 
  
  function appearInputOne(){
-  if (toggleEditable === 2){
+  if (toggleState === shelf.B1 && toggleEditable === 2){
     inputB1.style.display = "block";
     inputB2.style.display = "none"; 
     inputD.style.display = "none"; 
   }
-  else if (toggleEditable === 2.1){
+  else if (toggleState === shelf.B2 && toggleEditable === 2.1){
     inputB2.style.display = "block"; 
     inputD.style.display = "none"; 
     inputB1.style.display = "none"; 
   }
-  else if (toggleEditable === 4){
+  else if (toggleState === shelf.D && toggleEditable === 4){
    inputD.style.display = "block"; 
    inputB1.style.display = "none"; 
    inputB2.style.display = "none"; 
   }
+  else if (toggleState === shelf.D2 && toggleEditable === 4.1){
+    inputD2.style.display = "block";
+    inputD.style.display = "none"; 
+    inputB1.style.display = "none"; 
+    inputB2.style.display = "none"; 
+   }
 }; 
 
   const bodyEvent = document.body.addEventListener('keydown',(event) => {
@@ -106,6 +154,8 @@ let data = [];
         quantity: quantitySupplement_D
        });
     }
+
+
       if (toggleEditable === 2){
         inputB1.style.display = "none"; 
         inputOneB1.innerHTML = inputB1.value;
@@ -127,11 +177,6 @@ let data = [];
    }
   );
    
-   const shelf = {
-    B1: 'B1',
-    B2: 'B2',
-    D: 'D'
-   };
 
    let toggleState;
    
@@ -141,8 +186,12 @@ let data = [];
       shelf_B1.classList.add("shelf-active");
       shelf_B2.classList.remove("shelf-active");
       shelf_D.classList.remove("shelf-active");
+      shelf_D2.classList.remove("shelf-D2-active");
+
+      toggleEditable = 2;
       toggleState = shelf.B1;
       console.log(toggleState);
+      console.log(`toggleEditable ${toggleEditable}`);
     };
     hightLight_B1();
   });
@@ -153,8 +202,12 @@ let data = [];
       shelf_B2.classList.add("shelf-active");
       shelf_B1.classList.remove("shelf-active");
       shelf_D.classList.remove("shelf-active");
+      shelf_D2.classList.remove("shelf-D2-active");
+
+      toggleEditable = 2.1;
       toggleState = shelf.B2;
       console.log(toggleState);
+      console.log(`toggleEditable ${toggleEditable}`);
     };
     hightLight_B2();
   });
@@ -165,15 +218,38 @@ let data = [];
       shelf_D.classList.add("shelf-active");
       shelf_B1.classList.remove("shelf-active");
       shelf_B2.classList.remove("shelf-active");
+      shelf_D2.classList.remove("shelf-D2-active");
+      
+      toggleEditable = 4;
       toggleState = shelf.D;
       console.log(toggleState);
+      console.log(`toggleEditable ${toggleEditable}`);
     };
     hightLight_D();
+  });
+  
+  
+  const shelf_D2 = document.querySelector('.shelf-D2');
+
+  numbersD2.addEventListener('click', () => {
+    const hightLight_D2 = () => {
+      shelf_D2.classList.add("shelf-D2-active");
+      shelf_B1.classList.remove("shelf-active");
+      shelf_B2.classList.remove("shelf-active");
+      shelf_D.classList.remove("shelf-active");
+
+      toggleEditable = 4.1;
+      toggleState = shelf.D2;
+      console.log(toggleState);
+      console.log(`toggleEditable ${toggleEditable}`);
+    };
+    hightLight_D2();
   });
 
   let shelfQuantity_B1 = document.querySelector('.js-quantity-B1');
   let shelfQuantity_B2 = document.querySelector('.js-quantity-B2');
   let shelfQuantity_D = document.querySelector('.js-quantity-D');
+  let shelfQuantity_D2 = document.querySelector('.panelNumber-Quantity');
 
   shelfQuantity_B1.value = '';
   shelfQuantity_B2.value = '';
@@ -182,14 +258,15 @@ let data = [];
   shelfQuantity_B1.innerHTML = `0`;
   shelfQuantity_B2.innerHTML = `0`;
   shelfQuantity_D.innerHTML = `0`;
+  shelfQuantity_D2.innerHTML = '0';
 
-
-  const buttonAdd = document.querySelectorAll('.js-button-plus-B, .js-button-plus-D, .js-button-plus');
+  //PLUS BUTTON
+  const buttonAdd = document.querySelectorAll('.js-button-plus-B, .js-button-plus-D, .js-button-plus-D2');
 
   buttonAdd.forEach(add => {
     add.addEventListener('click',buttonPlus);
   }
-  )
+  );
   function buttonPlus(){
     if (toggleState === shelf.B1){
     quantitySupplement_B1++;
@@ -206,12 +283,22 @@ let data = [];
       shelfQuantity_D.innerHTML = `${quantitySupplement_D}`;
       console.log(quantitySupplement_D);
     }
+    else if (toggleState === shelf.D2){
+      quantitySupplement_D2++;
+      shelfQuantity_D2.innerHTML = `${quantitySupplement_D2}`;
+      console.log(quantitySupplement_D2);
+
+      //
+     
+
+    }
     else {
       console.log('none select');
     }
   };
 
-  const buttonSubtract = document.querySelectorAll('.js-button-minus-B, .js-button-minus-D');
+//SUBTRACT BUTTON
+  const buttonSubtract = document.querySelectorAll('.js-button-minus-B, .js-button-minus-D,.js-button-minus-D2');
 
   buttonSubtract.forEach(minus => {
     minus.addEventListener('click',buttonMinus); 
@@ -228,10 +315,15 @@ let data = [];
         shelfQuantity_B2.innerHTML = `${quantitySupplement_B2}`;
         console.log(quantitySupplement_B2);
       }
-      else if (toggleState === shelf.D && quantitySupplement_D > 0){
+      else if (toggleState === shelf.D &&   quantitySupplement_D > 0){
         quantitySupplement_D--;
         shelfQuantity_D.innerHTML = `${quantitySupplement_D}`;
         console.log(quantitySupplement_D);
+      }
+      else if (toggleState === shelf.D2 &&   quantitySupplement_D2 > 0){
+        quantitySupplement_D2--;
+        shelfQuantity_D2.innerHTML = `${quantitySupplement_D2}`;
+        console.log(quantitySupplement_D2);
       }
       else {
         alert('Reach Limit');
